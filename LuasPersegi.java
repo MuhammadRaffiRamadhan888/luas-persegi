886fe232385bf6583d5157cd805b290420aa8791
MuhammadRaffiRamadhan888 <raffiramadhan080908@gmail.com>
2026-09-21 14:13:18 +0800

    Add files via upload

---
+++ LuasPersegi.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tipedata;
+
+/**
+ *
+ * @author ACER
+ */
+public class LuasPersegi {
+    public static void main(String[] args) {
+        int sisi = 8;
+        int luas = sisi*sisi;
+        System.out.println("Luas persegi : " + luas);
+    }
+}
